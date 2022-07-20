6a8cb90e1dddf7d68981b203120779c6942fd837
peve <paulovictor237@gmail.com>
2022-07-20 14:20:18 -0300

    first-configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
