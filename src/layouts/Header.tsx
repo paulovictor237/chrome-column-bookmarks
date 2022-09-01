e85ebf99e3de20a8f270323ca8a662506e7cc6ca
Paulo Victor <paulovictor237@gmail.com>
2022-09-01 18:23:51 -0300

    new icons

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,12 +1,12 @@
-import { useRef } from "react";
-import Donate from "../components/ui/Donate";
-import NewTab from "../components/ui/NewTab";
-import SearchBar from "../components/ui/SearchBar";
+import { useRef } from 'react';
+import { FaGithub } from 'react-icons/fa';
+import { SiBuymeacoffee } from 'react-icons/si';
+import NewTab from '../components/ui/NewTab';
+import SearchBar from '../components/ui/SearchBar';
 
 export default function Header() {
   const searchBarRef = useRef<HTMLInputElement>(null);
   return (
-    //p-5 p-2 gap-0
     <div className="bg-dark700 flex items-center justify-between shadow-lg gap-2 p-2">
       <div className="flex gap-2 justify-between items-center h-full">
         {/* <DownloadJson /> */}
@@ -14,7 +14,30 @@ export default function Header() {
         {/* <LockEdit /> */}
       </div>
       <SearchBar />
-      <Donate />
+      <div className="flex gap-2 justify-between items-center h-full">
+        {' '}
+        <a
+          href="https://github.com/paulovictor237/chrome-column-tab"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub
+            className="text-neutral-200 hover:text-neutral-500 cursor-pointer"
+            size={28}
+          />
+        </a>
+        <a
+          href="https://www.buymeacoffee.com/peve"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <SiBuymeacoffee
+            size={28}
+            className="text-yellow-300 hover:text-yellow-500 cursor-pointer"
+          />
+        </a>
+      </div>
+
       {/* <span className=' inline-flex items-center select-none text-blue-700 border border-blue-700 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-1.5 text-center dark:border-blue-500 dark:text-blue-500 dark:focus:ring-blue-800'>
         Version: Beta
       </span> */}
--- src/layouts/TreeColumns.tsx
+++ src/layouts/TreeColumns.tsx
@@ -5,19 +5,28 @@ import { RootState } from '../store';
 
 export default function TreeColumns() {
   const columns = useSelector((state: RootState) => state.siteReducer.columns);
-  const searchFolder = useSelector((state: RootState) => state.siteReducer.searchFolder);
-  const searchBar = useSelector((state: RootState) => state.optionsReducer.searchBar);
+  const searchFolder = useSelector(
+    (state: RootState) => state.siteReducer.searchFolder
+  );
+  const searchBar = useSelector(
+    (state: RootState) => state.optionsReducer.searchBar
+  );
 
   return (
-    <div className=' overflow-x-auto w-full flex sc2 p-1 h-full'>
+    <div className=" overflow-x-auto w-full flex sc2 p-1 h-full">
       <AnimatePresence presenceAffectsLayout>
-        {searchBar && <Column key={-500} folder={searchFolder} index={-1}
-          status={searchFolder.children.length > 0 ? "Search" : "Search Empty"}
-        />}
+        {searchBar && (
+          <Column
+            key={-500}
+            folder={searchFolder}
+            index={-1}
+            status={searchFolder.children.length > 0 ? 'search' : 'no results'}
+          />
+        )}
         {columns.map((column, index) => {
-          return <Column key={column.id} folder={column} index={index} />
+          return <Column key={column.id} folder={column} index={index} />;
         })}
       </AnimatePresence>
     </div>
-  )
+  );
 }
