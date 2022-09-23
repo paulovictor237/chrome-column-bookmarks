88e43539a620003d398c9f01568022b8924bba7b
Paulo Victor <paulovictor237@gmail.com>
2022-09-23 10:34:52 -0300

    Settings

--- src/app/components/header/menu-left-icons/index.tsx
+++ src/app/components/header/menu-left-icons/index.tsx
@@ -5,7 +5,7 @@ import { ShowRecent } from '../components/recent';
 export const LeftIcons = () => {
   return (
     <section className="flex gap-2 justify-between items-center h-full">
-      <TitleTag title="Github" position="left">
+      <TitleTag title="Settings" position="left">
         <Settings />
       </TitleTag>
       <TitleTag title="Recent" position="left">
