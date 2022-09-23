01bc5eca96dbc641189d701da4ee29baefddd87d
Paulo Victor <paulovictor237@gmail.com>
2022-09-23 18:36:56 -0300

    enabled

--- src/app/components/header/submodules/menu-left-icons/index.tsx
+++ src/app/components/header/submodules/menu-left-icons/index.tsx
@@ -8,7 +8,7 @@ export const LeftIcons = () => {
   return (
     <section className="flex gap-2 justify-between items-center h-full">
       <TitleTag
-        title={newTab ? 'NewTab Activated' : 'NewTab Disabled'}
+        title={newTab ? 'NewTab Enabled' : 'NewTab Disabled'}
         position="left"
       >
         <NewTabMenu />
