e2f0d4829f8ff70ea3f18fde9b4a80232c87039d
Paulo Victor <paulovictor237@gmail.com>
2022-09-23 20:23:20 -0300

    new colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,16 +4,36 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'peve-zinc': '#71717a',
         'peve-light': '#272830',
         'peve-dark': '#1f2027',
         'peve-selected': '#198cf0',
-        'peve-zinc': '#71717a',
 
-        'warcraft-black': '#232527',
-        'warcraft-red': '#aa3420',
+        'starctaft-blue-strong': '#2971c5',
+        'starctaft-blue-light': '#a1b1c7',
+
+        'hearthstone-yellow': '#c87c22',
+        'hearthstone-brow': '#462c11',
+        'hearthstone-blue': '#30a1c6',
+
+        'diablo-blue': '#bbb5a1',
+        'diablo-blue': '#ba1a22',
+        'diablo-blue': '#000000',
+
+        'diablo3-blue': '#b7ad9a',
+        'diablo3-blue': '#be3314',
+        'diablo3-blue': '#390106',
+
         'warcraft-yellow': '#be953a',
-        'warcraft-blue': '#0b8cb0',
-        'warcraft-dark': '#0f1826',
+        'warcraft-red': '#aa3420',
+        'warcraft-black': '#232527',
+
+        'wow-yellow': '#c09634',
+        'wow-light': '#222b3b',
+        'wow-dark': '#141414',
+
+        'wow-classic-blue': '#0b8cb0',
+        'wow-classic-dark': '#0f1826',
 
         'dracula-Background': '#282a36',
         'dracula-Current-Line': '#44475a',
