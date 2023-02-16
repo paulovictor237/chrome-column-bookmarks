fec538a97d459eb8d9358a244a6904c77cb99c56
peve <paulovictor237@gmail.com>
2023-02-16 07:37:41 -0300

    fix bug when exclude a folder

--- src/app/components/operations/delete/index.tsx
+++ src/app/components/operations/delete/index.tsx
@@ -1,13 +1,19 @@
 import { Button } from '@/app/components/buttom';
 import Modal from '@/app/components/modal';
+import { useBookmarks } from '@/app/zustand/bookmarks';
 import { useContextMenu } from '@/app/zustand/context-menu';
+import { Site } from '@/domain/entities/site';
 import { chromeRemove } from '@/infra/services/chrome';
 import { Props } from './types';
 
 export const Delete = ({ isOpen, handleClose }: Props) => {
   const item = useContextMenu((state) => state.item);
+  const resetColumns = useBookmarks((state) => state.resetColumns);
+
   const handleAction = async () => {
-    item && chromeRemove(item.id);
+    const isFolder = !(item as Site).url;
+    if (isFolder) resetColumns(item?.parentId!);
+    chromeRemove(item?.id!);
     handleClose();
   };
   if (!item) return null;
