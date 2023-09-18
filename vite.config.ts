9eb2a1b640fff48ed9403ee224e8d2eeda68c252
Paulo Victor Duarte <paulovictor237@gmail.com>
2023-09-17 22:57:41 -0300

    bun

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import path from 'path';
-// import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // plugins: [react(), tsconfigPaths()],
   plugins: [react()],
   resolve: {
-    // alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-    alias: [{ find: '@', replacement: '/src' }],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
